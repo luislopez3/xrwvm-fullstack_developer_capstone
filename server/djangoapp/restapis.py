d6cbd5a1798d72e67774b4c13d6bfe37992c0d82
Luis Lopez <llopez1518@gmail.com>
2025-02-07 18:29:05 -0500

    attempt debug

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,19 +14,21 @@ sentiment_analyzer_url = os.getenv(
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
+    finally:
+        print("GET request call complete!")
 
 # function for retrieving sentiments
 def analyze_review_sentiments(text):
