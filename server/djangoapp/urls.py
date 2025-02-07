daee6303f1d63452efbb1cb2f64bfe3ccd8f33f8
Luis Lopez <llopez1518@gmail.com>
2025-02-07 16:07:30 -0500

    add some frontend services - need to debug others

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,32 +5,24 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # Path for registration
+    # path(route='register', view=views.registration, name='register'),
 
-    # path for login
+    # Path for login
     path(route='login', view=views.login_user, name='login'),
-
-    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer details
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-
-    # path for dealers
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    # Path for dealer reviews view
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
-    # path for dealer reviews
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
-    # path for add a review
+    # Path for adding a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
-    # path to get cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
